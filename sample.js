c0b2676f31cfec0478e7e58e8af19d759191c2fb
danielSoledad <danielalesole24@gmail.com>
2024-11-07 13:01:32 +0100

    Add samples.js for practice js fundamentals

---
+++ sample.js
@@ -0,0 +1,28 @@
+'use strict'
+//declarar las variables usando var(descontinuado) - let - const
+// let user
+// let age
+// let isStudent
+
+//asignacion
+// user = 'Pepe'
+// age = 22
+// isStudent = true
+
+//EL ESTANDAR ES: DECLARACIÓN + ASIGNACIÓN
+let user = 'Pepe'
+let age = 22
+//ejemplo de camelCase, se usa por CONVENIO
+let isStudent = true
+
+//Los LET se pueden REASIGNAR, no se pueden volver a declarar dentro del mismo módulo
+user = ' Jose '
+
+//CONST: No se pueden reasignar, se tienen que asignar al inicio sí o sí 
+
+const course = 'JavaScript'
+
+/* 
+Esto daría un error, ya que los CONST NO se pueden reasignar
+course = 'Python'
+*/
